eefb02ac55638a68cba03fdc43cffd6f3a5324a5
pvvdg <vladpetrachuk@gmail.com>
2022-02-01 17:27:01 +0300

    feat: add part of solution 2.2

---
+++ 2_2.ps1
@@ -0,0 +1,8 @@
+# Экспорт в JSON
+# a.	Экспортировать в JSON информацию о локальных дисках.
+# b.	Вывести содержимое файла на экран.
+# c.	Импортировать данные  из JSON (если есть возможность, то на другом компьютере, перенеся туда файл JSON) и вывести имя диска и свободное место (в ГБ и %)
+
+# Get-PSDrive -PSProvider FileSystem | ConvertTo-Json > "D:\Courses\powershell\day4\Tasks\2_2.json"
+Get-Content "D:\Courses\powershell\day4\Tasks\2_2.json"
+
