32e9afc221c4939eeaa09845bb192e6d9552bc63
pvvdg <vladpetrachuk@gmail.com>
2022-02-01 20:48:19 +0300

    feat: add solution 2.2

--- 2_2.ps1
+++ 2_2.ps1
@@ -3,7 +3,14 @@
 # b.	Вывести содержимое файла на экран.
 # c.	Импортировать данные  из JSON (если есть возможность, то на другом компьютере, перенеся туда файл JSON) и вывести имя диска и свободное место (в ГБ и %)
 
-# Get-PSDrive -PSProvider FileSystem | ConvertTo-Json > "D:\Courses\powershell\day4\Tasks\2_2.json"
-# Get-Content "D:\Courses\powershell\day4\Tasks\powershell_4_epam\2_2.json"
 
-$json = Get-Content "D:\Courses\powershell\day4\Tasks\powershell_4_epam\2_2.json" | ConvertFrom-Json
+$pathToFolder = 'G:\PowerShell\Day4\Tasks\powershell_4_epam\'
+
+# Get-PSDrive -PSProvider FileSystem | ConvertTo-Json > $($pathToFolder + '2_2.json') 
+# Get-Content $($pathToFolder + '2_2.json')
+
+$result = Get-Content $($pathToFolder + '2_2.json') | ConvertFrom-Json
+
+foreach ($disk in $result) {
+    Write-Host "Disk $($disk.Name), Free: $([math]::round($disk.Free / 1Gb,2)) Gb, Free: $([math]::round(100-$($disk.Used*100)/($disk.Free + $disk.Used))) %"
+}
